e28c196131c1e5a4b233da4d18a3b5359bedb85d
Arsenoverkill <arsenprogrammer007@gmail.com>
2024-07-24 13:34:27 +0600

    Favorite/basket (#3)
    * basket/favorite
    
    * f/b

--- src/appPage/site/component/layout/Header/Header.tsx
+++ src/appPage/site/component/layout/Header/Header.tsx
@@ -2,13 +2,30 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import scss from "./Header.module.scss";
 
 const Header: FC = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
   const router = useRouter();
+
+  const handleScroll = () => {
+    if (window.scrollY > 1) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className={scss.Home}>
+    <div className={`${scss.Header} ${isScrolled ? scss.scrolled : ""}`}>
       <Image
         onClick={() => router.push("/")}
         width={200}
