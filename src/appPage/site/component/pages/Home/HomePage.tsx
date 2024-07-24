a3b164ead91526817cffdce0a986b4a70ac8bc9b
Arsenoverkill <arsenprogrammer007@gmail.com>
2024-07-24 13:47:06 +0600

    f/basket

--- src/appPage/site/component/pages/Home/HomePage.tsx
+++ src/appPage/site/component/pages/Home/HomePage.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import scss from "./Home.module.scss";
 
 const Home = () => {
-  return <div className={scss.Home}></div>;
+  return <div></div>;
 };
 
 export default Home;
